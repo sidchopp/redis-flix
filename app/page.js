68d31b3105151f587dc86a399664ab0282049990
sidchopp <siddchopp@gmail.com>
2024-04-16 16:23:19 -0600

    renamed to redis next flix

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        <span class="text-blue-600 dark:text-blue-500">Redis</span> Flix
+        <span class="text-blue-600 dark:text-blue-500">Redis</span> Next Flix
       </h1>
     </main>
   );
